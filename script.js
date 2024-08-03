a9ab5498af496ed1321c2cad1b09f8a4998fc227
willian-matheus <85637800+williamtesche@users.noreply.github.com>
2024-08-03 13:30:56 -0300

    Feito a tela de cadastro

--- script.js
+++ script.js
@@ -1,37 +1,41 @@
 function storeLoginPassword() {
-    const password = document.getElementById('login-password').value;
-    console.log('Login Password:', password);
-    // Aqui você pode armazenar a senha em um local seguro
-  }
-  
-  function storeSignupPassword() {
-    const firstName = document.getElementById('signup-firstname').value;
-    const lastName = document.getElementById('signup-lastname').value;
-    const email = document.getElementById('signup-email').value;
-    const password = document.getElementById('signup-password').value;
-    const confirmPassword = document.getElementById('confirm-password').value;
-  
-    if (password !== confirmPassword) {
+  const password = document.getElementById('signup-password').value;
+  console.log('Login Password:', password);
+  // Aqui você pode armazenar a senha em um local seguro
+}
+
+function storeSignupPassword() {
+  const firstName = document.getElementById('signup-firstname').value;
+  const lastName = document.getElementById('signup-lastname').value;
+  const email = document.getElementById('signup-email').value;
+  const password = document.getElementById('signup-password').value;
+  const confirmPassword = document.getElementById('confirm-password').value;
+
+  if (password !== confirmPassword) {
       alert('As senhas não coincidem!');
       return;
-    }
-  
-    console.log('Signup Information:', {
+  }
+
+  console.log('Signup Information:', {
       firstName,
       lastName,
       email,
       password
-    });
+  });
+
+  // Aqui você pode armazenar a senha em um local seguro
+}
+
+function togglePasswordVisibility() {
+  const passwordField = document.getElementById('signup-password');
+  const confirmPasswordField = document.getElementById('confirm-password');
+  const toggleCheckbox = document.getElementById('toggle-password');
   
-    // Aqui você pode armazenar a senha em um local seguro
-  }
-  
-  function togglePasswordVisibility() {
-    const passwordField = document.getElementById('signup-password');
-    if (passwordField.type === 'password') {
+  if (toggleCheckbox.checked) {
       passwordField.type = 'text';
-    } else {
+      confirmPasswordField.type = 'text';
+  } else {
       passwordField.type = 'password';
-    }
+      confirmPasswordField.type = 'password';
   }
-  
+}
